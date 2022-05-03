c24c9d8176f19a7ba1fa989b4fa1e75fb464f1fb
Beau Elliott <belliott1516@gmail.com>
2022-05-03 14:27:27 -0700

    created header file, added state, created h1 in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+import Header from './Components/Header';
 import './App.css';
 
 function App() {
+  // set state
+  const [instructions, setInstructions] = useState('');
+  const [name, setName] = useState('Customer');
+  const [foodID, setFoodID] = useState(1);
+  const [sideID, setSideID] = useState(1);
+  const [drinkID, setDrinkID] = useState(1); 
+
   return (
     <div className="App">
       <header className="App-header">
+        <Header text={'Kiki\'s Delivery Service'} />
       </header>
       <main>
+        <h1>Welcome {name} please place your order here:</h1>
       </main>
     </div>
   );
