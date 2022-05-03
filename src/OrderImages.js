a5a820d184a93d07768c319fe63b89c067ab80ce
Beau Elliott <belliott1516@gmail.com>
2022-05-03 15:41:32 -0700

    adjusted formatting of orderImages.js and app.css to make images more uniform

--- src/OrderImages.js
+++ src/OrderImages.js
@@ -3,9 +3,15 @@ import React from 'react';
 export default function OrderImages({ foodID, drinkID, sideID }) {
   return (
     <div className='order-images'>
-      <img src={`./assets/side-${sideID}.png`} />
-      <img src={`./assets/food-${foodID}.png`} />
-      <img src={`./assets/drink-${drinkID}.png`} />
+      <div className='order-column'>Side
+        <img src={`./assets/side-${sideID}.png`} />  
+      </div>
+      <div className='order-column'>Main
+        <img src={`./assets/food-${foodID}.png`} />
+      </div>
+      <div className='order-column'>Drink
+        <img src={`./assets/drink-${drinkID}.png`} /> 
+      </div>
     </div>
   );
 }
