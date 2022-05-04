6f7b089dda07f22f64f8abbb371cb8e1010dc7a0
Beau Elliott <belliott1516@gmail.com>
2022-05-04 10:24:40 -0700

    added css and adjusted fonts

--- src/OrderNameInput.js
+++ src/OrderNameInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function OrderNameInput({ setName }) {
   return (
-    <div className='name-input'> Please Type your name here:
+    <div className='name-input'> Please Type your name here: 
       <input onChange={(e) => setName(e.target.value)}>
       </input>
     </div>
